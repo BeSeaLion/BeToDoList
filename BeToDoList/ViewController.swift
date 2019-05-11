107a7c115b706379573278cc80ad171488436d5d
BeSeaLion <besealion@gmail.com>
2019-05-11 16:47:04 +0200

    Initial Commit

---
+++ BeToDoList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BeToDoList
+//
+//  Created by Josef Divín on 11/05/2019.
+//  Copyright © 2019 Josef Divín. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
